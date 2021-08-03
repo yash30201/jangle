edc09c7b51fb66013a6c7ffdfd2c628e96f99bee
yash <yash30301dwed@gasnjdas.com>
2021-08-03 20:46:02 +0530

    update socket url

--- src/context/socket.js
+++ src/context/socket.js
@@ -2,7 +2,7 @@ import socketio from 'socket.io-client';
 import React from 'react'
 
 
-const SOCKET_URL = 'http://localhost:5000/';
+const SOCKET_URL = 'https://jangle-api.herokuapp.com/';
 
 export const socket = socketio.connect(SOCKET_URL);
 export const socketContext = React.createContext();
