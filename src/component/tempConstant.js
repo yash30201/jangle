acf2b2cc0423b8527030653dfb7cabf0c19ba578
yash <yash30301dwed@gasnjdas.com>
2021-08-11 01:41:17 +0530

    downgraded socket.io-client to 2.4.0

--- src/component/tempConstant.js
+++ src/component/tempConstant.js
@@ -1,4 +1,5 @@
 const apiUrl = "https://jangle-api.herokuapp.com/";
+// const apiUrl = "http://localhost:5000/";
 
 const data = {apiUrl};
 export default data;
--- src/context/socket.js
+++ src/context/socket.js
@@ -3,6 +3,7 @@ import React from 'react'
 
 
 const SOCKET_URL = 'https://jangle-api.herokuapp.com/';
+// const SOCKET_URL = "http://localhost:5000/";
 
 export const socket = socketio.connect(SOCKET_URL);
 export const socketContext = React.createContext();
