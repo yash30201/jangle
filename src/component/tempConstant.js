e7cc2dcc01d1a571efe42f0e860cb6721d16da21
yash <yash30301dwed@gasnjdas.com>
2021-08-12 01:16:29 +0530

    Updated API urls

--- src/component/tempConstant.js
+++ src/component/tempConstant.js
@@ -1,5 +1,5 @@
-// const apiUrl = "https://jangle-api.herokuapp.com/";
-const apiUrl = "http://localhost:5000/";
+const apiUrl = "https://jangle-api.herokuapp.com/";
+// const apiUrl = "http://localhost:5000/";
 
 const data = {apiUrl};
 export default data;
